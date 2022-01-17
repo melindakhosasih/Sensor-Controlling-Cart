dcc5b918fe64749d99bfd02acacfb210521ae5a1
hoop__ <73927244+Hlinm-qq@users.noreply.github.com>
2022-01-18 00:42:11 +0800

    Update music_example.v

--- Sensor Controlling Cart/music_example.v
+++ Sensor Controlling Cart/music_example.v
@@ -21,19 +21,19 @@ module sound (
         if(en == 1) begin
             case(ibeatNum)
                 // --- Measure 1 ---
-                12'd0: toneR = `hb;      12'd1: toneR = `hb; // HG (half-beat)
+                12'd0: toneR = `hb;      12'd1: toneR = `hb;
                 12'd2: toneR = `hb;      12'd3: toneR = `hb;
                 12'd4: toneR = `hb;      12'd5: toneR = `hb;
                 12'd6: toneR = `hb;      12'd7: toneR = `hb;
-                12'd8: toneR = `hb;      12'd9: toneR = `hb; // HE (half-beat)
-                12'd10: toneR = `hb;     12'd11: toneR = `hb;
-                12'd12: toneR = `hb;     12'd13: toneR = `hb;
-                12'd14: toneR = `hb;     12'd15: toneR = `sil; // (Short break for repetitive notes: high E)
+                12'd8: toneR = `sil;      12'd9: toneR = `sil;
+                12'd10: toneR = `sil;     12'd11: toneR = `sil;
+                12'd12: toneR = `sil;     12'd13: toneR = `sil;
+                12'd14: toneR = `sil;     12'd15: toneR = `sil;
 
-                12'd16: toneR = `sil;     12'd17: toneR = `sil; // HE (one-beat)
-                12'd18: toneR = `sil;     12'd19: toneR = `sil;
-                12'd20: toneR = `sil;     12'd21: toneR = `sil;
-                12'd22: toneR = `sil;     12'd23: toneR = `sil;
+                12'd16: toneR = `hb;     12'd17: toneR = `hb;
+                12'd18: toneR = `hb;     12'd19: toneR = `hb;
+                12'd20: toneR = `hb;     12'd21: toneR = `hb;
+                12'd22: toneR = `hb;     12'd23: toneR = `hb;
                 12'd24: toneR = `sil;     12'd25: toneR = `sil;
                 12'd26: toneR = `sil;     12'd27: toneR = `sil;
                 12'd28: toneR = `sil;     12'd29: toneR = `sil;
@@ -49,19 +49,19 @@ module sound (
     always @(*) begin
         if(en == 1)begin
             case(ibeatNum)
-                12'd0: toneL = `hb;  	12'd1: toneL = `hb; // HC (two-beat)
+                12'd0: toneL = `hb;  	12'd1: toneL = `hb;
                 12'd2: toneL = `hb;  	12'd3: toneL = `hb;
                 12'd4: toneL = `hb;	    12'd5: toneL = `hb;
                 12'd6: toneL = `hb;  	12'd7: toneL = `hb;
-                12'd8: toneL = `hb;	    12'd9: toneL = `hb;
-                12'd10: toneL = `hb;	12'd11: toneL = `hb;
-                12'd12: toneL = `hb;	12'd13: toneL = `hb;
-                12'd14: toneL = `hb;	12'd15: toneL = `hb;
+                12'd8: toneL = `sil;	12'd9: toneL = `sil;
+                12'd10: toneL = `sil;	12'd11: toneL = `sil;
+                12'd12: toneL = `sil;	12'd13: toneL = `sil;
+                12'd14: toneL = `sil;	12'd15: toneL = `sil;
 
-                12'd16: toneL = `sil;     12'd17: toneL = `sil; // HE (one-beat)
-                12'd18: toneL = `sil;     12'd19: toneL = `sil;
-                12'd20: toneL = `sil;     12'd21: toneL = `sil;
-                12'd22: toneL = `sil;     12'd23: toneL = `sil;
+                12'd16: toneL = `hb;     12'd17: toneL = `hb;
+                12'd18: toneL = `hb;     12'd19: toneL = `hb;
+                12'd20: toneL = `hb;     12'd21: toneL = `hb;
+                12'd22: toneL = `hb;     12'd23: toneL = `hb;
                 12'd24: toneL = `sil;     12'd25: toneL = `sil;
                 12'd26: toneL = `sil;     12'd27: toneL = `sil;
                 12'd28: toneL = `sil;     12'd29: toneL = `sil;
